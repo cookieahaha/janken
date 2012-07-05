fa239be6a6e16292da6a6e5767618b64371e5434
Yasukazu Kashii <cookieahaha@gmail.com>
2012-07-05 16:12:17 -0700

    userhandler thread

--- src/main/java/me/kukkii/janken/GameManager.java
+++ src/main/java/me/kukkii/janken/GameManager.java
@@ -3,17 +3,13 @@ package me.kukkii.janken;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class GameManager{
 
   int port = 50000; 
-  int timeout_msec = 10000; // accept() のタイムアウトは10秒にしてみる。
-  private int mountOfGames;
+//  int timeout_msec = 10000; // accept() のタイムアウトは10秒にしてみる。
+  private int amountOfGames;
   private Socket sock;
 
   public static void main(String[] args) throws Exception{
@@ -27,26 +23,14 @@ public class GameManager{
 
   public void makeGame() throws Exception{
     ServerSocket svsock = new ServerSocket(port);
-    svsock.setSoTimeout(timeout_msec);  // ※必要があるときのみ設定する。
+//    svsock.setSoTimeout(timeout_msec);  // ※必要があるときのみ設定する。
 
-    sock = svsock.accept();
-    InputStream is = sock.getInputStream();
-    System.out.println("jan ken pon!");
-    DataInputStream input = new DataInputStream(is);
-    int userHand = input.readInt();
-    int botHand = (int)(Math.random()*3);
-    int result = 100;
-    if(userHand==botHand){
-      result = 0;
+    while(true){
+      sock = svsock.accept(); 
+      Thread uh = new Thread(new UserHandler(sock));
+      uh.start();
+      amountOfGames += 1;
     }
-    if((userHand==0 && botHand==1) || (userHand==1 && botHand==2) || (userHand==2 && botHand==0)){
-      result = 1;
-    }
-    if((userHand==0 && botHand==2) || (userHand==1 && botHand==0) || (userHand==2 && botHand==1)){
-      result = 2;
-    }
-    DataOutputStream os = new DataOutputStream(sock.getOutputStream());
-    os.writeInt(result);
 
   }
 
---
+++ src/main/java/me/kukkii/janken/UserHandler.java
@@ -0,0 +1,45 @@
+package me.kukkii.janken;
+
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException; 
+
+
+public class UserHandler implements Runnable{
+  
+  private Socket sock;
+
+  public UserHandler(Socket sock){
+    this.sock = sock;
+  }
+
+  public void run(){
+    try{
+      InputStream is = sock.getInputStream();
+      System.out.println("jan ken pon!");
+      DataInputStream input = new DataInputStream(is);
+      int userHand = input.readInt();
+      int botHand = (int)(Math.random()*3);
+      int result = 100;
+      if(userHand==botHand){
+        result = 0;
+      }
+      if((userHand==0 && botHand==1) || (userHand==1 && botHand==2) || (userHand==2 && botHand==0)){
+        result = 1;
+      }
+      if((userHand==0 && botHand==2) || (userHand==1 && botHand==0) || (userHand==2 && botHand==1)){
+        result = 2;
+      }
+      DataOutputStream os = new DataOutputStream(sock.getOutputStream());
+      os.writeInt(result);
+    }
+    catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+
+}
