98731c0caaa0b31fe7ddbaa89a10c8b23613e197
Yasukazu Kashii <cookieahaha@gmail.com>
2012-07-05 18:27:14 -0700

    jankenclient

--- src/main/java/me/kukkii/janken/JankenClient.java
+++ src/main/java/me/kukkii/janken/JankenClient.java
@@ -11,32 +11,52 @@ public class JankenClient{
   
   private static String host ="localhost";
   private static int port = 50000;
+  private DataOutputStream out;
+  private DataInputStream in;
 
-  public static void main(String[] args){
+  public JankenClient(){
     try{
       Socket sock = new Socket();
       sock.connect(new InetSocketAddress(host,port));     
-      DataOutputStream out = new DataOutputStream(sock.getOutputStream());
+      out = new DataOutputStream(sock.getOutputStream());
       InputStream is = sock.getInputStream();
-      DataInputStream in = new DataInputStream(is);
-      while(true){
-        out.writeInt(1);   // choki 4testing
-        int result = in.readInt();
-        if(result == 0){
-          System.out.println("tie");
-        }
-        if(result == 1){
-          System.out.println("u win");
-        }
-        if(result == 2){
-          System.out.println("u lose");
-        } 
-      }
+      in = new DataInputStream(is);
     }
-    catch(Exception e){
-    
+    catch (Exception e){
+    }
+  }  
+
+  public static void main(String[] args){
+    JankenClient jc = new JankenClient();
+    while(true){
+      int result = jc.game(Hand.ROCK);
+      jc.showResult(result);
     }
   }
 
+  public int game(Hand hand){
+    try{
+      out.writeInt(hand.value());   // choki 4testing
+      int result = in.readInt();
+      return result;
+    }
+    catch(Exception e){
+      return -1;
+    }
+  }
 
+  public static void showResult(int result){
+    if(result == -1){
+      System.err.println("server error");
+    }
+    if(result == 0){
+      System.out.println("tie");
+    }
+    if(result == 1){
+      System.out.println("u win");
+    }  
+    if(result == 2){
+      System.out.println("u lose");
+    } 
+  }
 }
