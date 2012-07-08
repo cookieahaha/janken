4c5285fa7c409fe99c759829c398c2f3456e55da
Yasukazu Kashii <cookieahaha@gmail.com>
2012-07-08 16:36:04 -0700

    mysql

--- src/main/java/me/kukkii/janken/log/LogManager.java
+++ src/main/java/me/kukkii/janken/log/LogManager.java
@@ -3,13 +3,23 @@
 package me.kukkii.janken.log;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
 
 public class LogManager {
 
-  public LogManager() {
-  }
+  private Connection connection;
 
-  public void write(LogItem item) throws IOException {
+  public LogManager() throws Exception {
+    Class.forName("com.mysql.jdbc.Driver");
+    connection=DriverManager.getConnection("jdbc:mysql://localhost/janken","root","");  // localhost ÇÃïîï™ÇÅ@ÉTÅ[ÉoÅ[ÇÃñºëOÇ©ÅAIPaddress Ç…Ç©Ç¶ÇÈ
+  }		
+
+
+  public void write(LogItem item) throws Exception {
+    Statement statement=connection.createStatement();
+    statement.execute("INSERT INTO logtable(timestamp, user,bot,userHand,botHand,result) values (" + item.getTimestamp() + "," + item.getUser().getName() + "," + item.getBot().getName() + "," + item.getUserHand().value() + "," + item.getBotHand().value() + "," + item.getResult().value() + ");");
   }
 
 }
